374abdcb48e4ca89abb102bf5803b3b224079237
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2024-10-10 00:17:42 +0100

    build fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Overhead from "@/components/overview/Overhead";
 import { Overview } from "@/components/overview/Overview";
-import OverviewProduct from "@/components/overview/overviewProduct";
+import OverviewProduct from "@/components/overview/OverviewProduct";
 import OverviewTable from "@/components/overview/overviewTable/page";
 import OverviewSummary from "@/components/OverviewSummary";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
